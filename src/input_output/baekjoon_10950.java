baa3f279f090ab4d358c428b9c184c42185f1bc2
GH <mintaka04orion@naver.com>
2022-12-05 20:04:55 +0900

    EOF처리

--- src/input_output/baekjoon_10950.java
+++ src/input_output/baekjoon_10950.java
@@ -3,6 +3,7 @@ package input_output;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class baekjoon_10950 {
 	public static void main(String[] args) throws IOException {
@@ -10,15 +11,26 @@ public class baekjoon_10950 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int num = Integer.parseInt(br.readLine());
 		
+//		for(int i = 0; i < num; i++) {
+//			int a = System.in.read();
+//			int b = System.in.read();
+//			int c = System.in.read();
+//			br.read();
+//			System.out.println("a : " + a);
+//			System.out.println("b : " + b);
+//			System.out.println("c : " + c);
+//			System.out.println(a - 48 + c - 48);
+//		}
+// cr lf가 따로 읽히므로 안됨
+		
+		StringBuilder sb = new StringBuilder();
+		
 		for(int i = 0; i < num; i++) {
-			int a = System.in.read();
-			int b = System.in.read();
-			int c = System.in.read();
-			br.read();
-			System.out.println("a : " + a);
-			System.out.println("b : " + b);
-			System.out.println("c : " + c);
-			System.out.println(a - 48 + c - 48);
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+			sb.append("\n");
 		}
+		System.out.println(sb);
+		
 	}
 }
---
+++ src/input_output/baekjoon_10951.java
@@ -0,0 +1,24 @@
+package input_output;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon_10951 {
+	public static void main(String[] args) throws IOException {
+		
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String a;
+		
+		while( (a = br.readLine()) != null) {
+			StringTokenizer st = new StringTokenizer(a);
+			sb.append(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())).append("\n");
+			
+		}
+
+		System.out.println(sb.toString());
+		
+	}
+}
