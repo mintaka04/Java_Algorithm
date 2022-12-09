99ca124a9c15125d4563a4381555194df8a55b3f
GH <mintaka04orion@naver.com>
2022-12-09 15:18:07 +0900

    12.09 별찍기-string에서 잘라 사용

---
+++ src/input_output/Baekjoon_10991.java
@@ -0,0 +1,27 @@
+package input_output;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_10991 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		String space = " ";
+		String star = "*";
+		
+		for(int i = 1; i < num; i++) {
+			star += " *";
+			space += " ";
+		}
+		for(int i = 1; i <= num; i++) {
+			sb.append(space.substring(0, num-i)).append(star.substring(0, 2*i-1));
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
