bb9dd6fe06c97b0092086ef2dd6f37640d06d8f7
GH <mintaka04orion@naver.com>
2022-12-10 23:28:08 +0900

    12.10 memoization 추가

--- src/dynamic_programming/Baekjoon_1463.java
+++ src/dynamic_programming/Baekjoon_1463.java
@@ -3,20 +3,28 @@ package dynamic_programming;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Baekjoon_1463 {
+	
+	static int dp[] = new int[1000001];
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
+		Arrays.fill(dp, -1);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int num = Integer.parseInt(br.readLine());
 		int result = make_one(num);
 		
 		System.out.println(result);
-		
-		
+				
 	}
 	
 	static int make_one(int num) {
+		
+		if(dp[num] != -1) {
+			return dp[num];
+		}
 		
 		if(num == 1) {
 			
@@ -24,18 +32,16 @@ public class Baekjoon_1463 {
 			
 		}else {
 			
-			int before = make_one(num-1) + 1;			
-			int[] numArrary = {before, before, before};
+			dp[num] = make_one(num-1) + 1;			
 			
 			if(num%3 == 0) {
-				numArrary[0] = make_one(num/3) + 1;
+				dp[num] = Math.min(dp[num], make_one(num/3) + 1);
 			}
 			if(num%2 == 0) {
-				numArrary[1] = make_one(num/2) + 1;
+				dp[num] = Math.min(dp[num], make_one(num/2) + 1);
 			}
 			
-			return Math.min(numArrary[0], Math.min(numArrary[1], numArrary[2]));
-			
+			return dp[num];
 		}
 	}
 }
